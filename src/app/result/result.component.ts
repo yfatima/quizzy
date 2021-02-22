58e65d30d319e448f743d2c28b9ad4aca751ca38
Svic579 <54674401+Svic579@users.noreply.github.com>
2021-02-22 16:10:20 -0500

    added colors and bevereage image

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
-  styles: [
-  ]
+  styleUrls: ['./result.component.css']
 })
 export class ResultComponent implements OnInit {
 
